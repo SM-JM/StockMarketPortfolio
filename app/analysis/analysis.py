56f5527c45e0b5232a07d6767a6b56adf5176dcf
Shane Miller <shortyute@gmail.com>
2021-08-16 11:02:13 -0500

    Completed price change and price prediction functionality

--- app/analysis/analysis.py
+++ app/analysis/analysis.py
@@ -23,13 +23,61 @@ analysis_bp = Blueprint(
 @analysis_bp.route("/priceChange", methods=["GET"])
 def priceChange():
 	
-	createPriceChangeGraph(symbol="CCC",startDate="2018-01-01",endDate="2019-01-01")
+	pCreatedModelNames =  getCreatedModelSymbolsNames()
 	
 	return render_template(
-		#"graph.html"
-		"priceChange.jinja2.html", hReturn=0.00
+		"priceChange.jinja2.html", 
+		hSymbols=pCreatedModelNames,
+		hReturn=""
     )
     
+@analysis_bp.route("/priceChangeSubmit", methods=["GET"])
+def priceChangeSubmit():
+	
+	# Populate dropdown 
+	pCreatedModelNames 		=  getCreatedModelSymbolsNames()
+	
+	## Get input from user
+	symbol 					= request.args.get("symbol")
+	
+	pStartDate				= request.args.get("sDate")
+	pStartDate				= datetime.strptime(pStartDate, '%Y-%m-%d')
+
+	pEndDate				= request.args.get("eDate")
+	pEndDate				= datetime.strptime(pEndDate, '%Y-%m-%d')
+	
+	pAllPrices				= request.args.get("isReturnAllPrices")
+	
+	if symbol is not None:
+		## Get data to calculate price change
+		df 						= getPriceData(symbol)
+		
+		df["Date"] 				= pd.to_datetime(df["Date"], infer_datetime_format=True)
+
+		df.set_index(df.Date, inplace=True)
+
+		dateList = df.Date.tolist()
+		if (pStartDate not in dateList) or (pEndDate not in dateList):
+			pReturn = "Error, no trades done on both dates indicated."
+		else:
+			## Get the startPrice and endPrice as a single value
+			startPrice = (df.loc[df.Date==pStartDate,["Close_Price"]]["Close_Price"].tolist())[0]
+			endPrice   = (df.loc[df.Date==pEndDate,  ["Close_Price"]]["Close_Price"].tolist())[0]
+			
+			# Initialise percentChange then calculate if values are valid
+			percentChange = 0.0
+			if startPrice > 0.0 and endPrice > 0.0:
+				percentChange = (endPrice-startPrice)/startPrice
+			pReturn = "{:.2%}".format(percentChange)
+			createPriceChangeGraph(symbol=symbol,startDate=pStartDate,endDate=pEndDate,allPrices=pAllPrices)
+	else:
+		pReturn = "Error, no symbol selected or invalid symbol!"
+	return render_template(
+		"priceChange.jinja2.html", 
+		hSymbols=pCreatedModelNames,
+		hReturn=pReturn
+    )	
+	
 @analysis_bp.route("/pricePrediction", methods=["GET"])
 def pricePrediction():
 
@@ -50,15 +98,13 @@ def pricePredictionSubmit():
 	symbol 					= request.args.get("symbol")
 	pIsUseSentimentModels	= request.args.get("sentiment")
 	
-	print("[{}] is the symbol".format(symbol))
-	print("[{}] is the use sentiment option selected?".format(pIsUseSentimentModels))
-	
 	pCreatedModelNames 		= getCreatedModelSymbolsNames()
 	
-	pPrediction 		= lstm_model_load(symbol,pIsUseSentimentModels)
-	
-	createPredictionGraph(symbol,pPrediction)
-	
+	if symbol is not None:
+		pPrediction 		= lstm_model_load(symbol,pIsUseSentimentModels)
+		createPredictionGraph(symbol,pPrediction)
+	else:
+		pPrediction = ""
 	return render_template(
         "pricePrediction.jinja2.html",
 		hIsSentimentModel=pIsUseSentimentModels,
@@ -122,7 +168,6 @@ def createPredictionGraph(symbol,prediction):
 						tooltips=[('Date', '@z'), ('Price', '@y')]
 			)
 	
-	
 	p.line(x='x', y='y', source=source, legend_label="Price History", line_color="royalblue", line_width=2)
 	p.circle('x', y='y', source=source, legend_label="Price History",  color="grey", size=4)
 
@@ -141,27 +186,34 @@ def createPredictionGraph(symbol,prediction):
 	show(p)
 	return
 
-def createPriceChangeGraph(symbol,startDate,endDate):
-	# output to file
+def createPriceChangeGraph(symbol,startDate,endDate,allPrices):
+	# Set file to output graph to
 	output_file('./app/analysis/templates/graph.html')  # Render to static HTML
 	
 	# Get data for graph
 	df = getPriceData(symbol)
 	
+	# If allPrices not specified then filter dataframe by date range
+	if allPrices != "True":
+		df["Date"] = pd.to_datetime(df["Date"], infer_datetime_format=True) #Convert to datetime
+		df = df[(df.Date >= startDate) & (df.Date <= endDate)] #Filter
+		z = df["Date"].dt.strftime('%Y-%m-%d') # Get filtered dates as string for tooltip
+	else:
+		z = df["Date"] # No filtering is need so assign string dates to z before conversion to datetime
+		
 	x 	= pd.to_datetime(df["Date"], infer_datetime_format=True)
 	x 	= x.tolist()
-	datesX  = x #pd.date_range(start='2016-01-01', end='2020-12-31')
+	datesX  = x 
 	valuesY = pd.DataFrame(df["Close_Price"].tolist(), columns=["A"])
-	z = df["Date"]
 
-	# keep track of the unchanged, y-axis values
 	source  = ColumnDataSource(data={'x': datesX, 'y': valuesY['A'], 'z':z}) 	
-	
+		
 	hover = HoverTool(
 		tooltips=[('Date', '@z'), ('Price', '@y')])
 		
 	# Create figure
-	p = figure(	title="Price History for "+symbol,
+	gTitle = "Price History for "+symbol+" ("+str(startDate)[0:10]+" to "+str(endDate)[0:10]+")"
+	p = figure(	title=gTitle,
 				x_axis_label="Date", x_axis_type='datetime',
 				y_axis_label="Price",
 				plot_width = 1500, plot_height = 400,
@@ -170,7 +222,7 @@ def createPriceChangeGraph(symbol,startDate,endDate):
 	   
 	# add a line renderer
 	p.line(x='x', y='y', source=source, legend_label="Price History", line_color="royalblue", line_width=2)
-	p.circle('x', y='y', source=source, legend_label="Price History",  color="grey", size=2)
+	p.circle('x', y='y', source=source, legend_label="Price History",  color="grey", size=4)
 
 	p.add_tools(hover)
 
@@ -180,6 +232,8 @@ def createPriceChangeGraph(symbol,startDate,endDate):
 	p.axis.axis_label_text_font_style = 'bold italic'
 	p.xaxis.axis_label_text_font_size = "16pt"
 	p.yaxis.axis_label_text_font_size = "16pt"
+	p.legend.location = "top_left"
+
 	
 	# show the results
 	show(p)	
