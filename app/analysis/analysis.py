f483940a7d03e241b93f3d0da5e4774542feb774
Shane Miller <shortyute@gmail.com>
2021-08-15 22:06:52 -0500

    Refined stock price prediction and added price change functionality

--- app/analysis/analysis.py
+++ app/analysis/analysis.py
@@ -1,7 +1,12 @@
 from flask 			import Blueprint, render_template, request
 from bokeh.plotting import figure, output_file, show
-from datetime import datetime
+from bokeh.models import ColumnDataSource, DatetimeTickFormatter, HoverTool
+from bokeh.models.widgets import DateRangeSlider
+from bokeh.layouts import layout, column
+from bokeh.models.callbacks import CustomJS
+from bokeh.plotting import figure, output_file, show, save
 
+from datetime import datetime
 
 from .lstm_model_load	import *
 from .create_lstm_model_multi_variate import *
@@ -17,9 +22,12 @@ analysis_bp = Blueprint(
 @analysis_bp.route("/", methods=["GET"])
 @analysis_bp.route("/priceChange", methods=["GET"])
 def priceChange():
-
-    return render_template(
-        "priceChange.jinja2.html",hReturn=0.00
+	
+	createPriceChangeGraph(symbol="CCC",startDate="2018-01-01",endDate="2019-01-01")
+	
+	return render_template(
+		#"graph.html"
+		"priceChange.jinja2.html", hReturn=0.00
     )
     
 @analysis_bp.route("/pricePrediction", methods=["GET"])
@@ -47,10 +55,9 @@ def pricePredictionSubmit():
 	
 	pCreatedModelNames 		= getCreatedModelSymbolsNames()
 	
-	#pPrediction = 0
 	pPrediction 		= lstm_model_load(symbol,pIsUseSentimentModels)
 	
-	createGraph(symbol,pPrediction)
+	createPredictionGraph(symbol,pPrediction)
 	
 	return render_template(
         "pricePrediction.jinja2.html",
@@ -67,20 +74,24 @@ def stockRelationship():
 	return render_template(
 		"stockRelationship.jinja2.html"
 	)
+def getPriceData(symbol):
 
-def createGraph(symbol,prediction):
-	# output to file
-	output_file('./app/analysis/templates/graph.html')  # Render to static HTML
-	  
-	  
-	  
 	#### Load dataset
 	df 	= pd.read_csv(	os.path.join('./app/analysis/historical_data/dataset',
 								'prices_sentiment_'+symbol+'.csv'),
 								infer_datetime_format=True, 
 								sep=','
-		)  
-	# create figure
+		)  	
+	return df
+
+def createPredictionGraph(symbol,prediction):
+	# output to file
+	output_file('./app/analysis/templates/graph.html')  # Render to static HTML
+	  
+	# Get data for graph
+	df = getPriceData(symbol)
+	
+	# Create figure
 	p = figure(	title="Price Prediction and Price History for "+symbol,
 				x_axis_label="Date", x_axis_type='datetime',
 				y_axis_label="Price",
@@ -90,28 +101,87 @@ def createGraph(symbol,prediction):
 	# add a line renderer
 	x 	= pd.to_datetime(df["Date"], infer_datetime_format=True)
 	x 	= x.tolist()
-	x   = x[-500:] 
+	x   = x[-100:] # Limit the amount of dates in axis to keep graph easy to read
 	x.append(datetime(2021,1,4))
 	
 	y1 	= df["Close_Price"].tolist()
-	y1  = y1[-500:]
+	y1  = y1[-100:] # Limit the amount of prices in axis to keep graph easy to read
 	y1.append(np.nan)
 	
-	y2 	= [np.nan] * (len(y1)-1)
+	y2 	= [np.nan] * (len(y1)-1) # Used to ensure that x & y axis have same len
 	y2.append(prediction)
 	
-	print(y2[-5:])
+	z = df["Date"].tolist()
+	z = z[-100:]
+	z.append("2021-01-04")
 	
-	p.line(x,  y1, legend_label="Price History", line_color="royalblue", line_width=2)
-	p.circle(x, y2, legend_label="Prediction",    color="goldenrod", size=6)
+	source   = ColumnDataSource(data={'x': x, 'y': y1, 'z':z}) 	
+	source2  = ColumnDataSource(data={'x': x, 'y': y2, 'z':z}) 	
+
+	hover = HoverTool(
+						tooltips=[('Date', '@z'), ('Price', '@y')]
+			)
 	
+	
+	p.line(x='x', y='y', source=source, legend_label="Price History", line_color="royalblue", line_width=2)
+	p.circle('x', y='y', source=source, legend_label="Price History",  color="grey", size=4)
+
+	p.circle('x', y='y', source=source2, legend_label="Prediction",    color="goldenrod", size=12)
+	p.add_tools(hover)
+	
+	#Formatting
+	p.title.text_font_size			  = '20pt'
+	p.axis.major_label_text_font_size = '15pt'
+	p.axis.axis_label_text_font_style = 'bold italic'
+	p.xaxis.axis_label_text_font_size = "16pt"
+	p.yaxis.axis_label_text_font_size = "16pt"
+	p.legend.location = "top_left"
+	
+	# show the results
+	show(p)
+	return
+
+def createPriceChangeGraph(symbol,startDate,endDate):
+	# output to file
+	output_file('./app/analysis/templates/graph.html')  # Render to static HTML
+	
+	# Get data for graph
+	df = getPriceData(symbol)
+	
+	x 	= pd.to_datetime(df["Date"], infer_datetime_format=True)
+	x 	= x.tolist()
+	datesX  = x #pd.date_range(start='2016-01-01', end='2020-12-31')
+	valuesY = pd.DataFrame(df["Close_Price"].tolist(), columns=["A"])
+	z = df["Date"]
+
+	# keep track of the unchanged, y-axis values
+	source  = ColumnDataSource(data={'x': datesX, 'y': valuesY['A'], 'z':z}) 	
+	
+	hover = HoverTool(
+		tooltips=[('Date', '@z'), ('Price', '@y')])
+		
+	# Create figure
+	p = figure(	title="Price History for "+symbol,
+				x_axis_label="Date", x_axis_type='datetime',
+				y_axis_label="Price",
+				plot_width = 1500, plot_height = 400,
+				tools="pan, wheel_zoom, box_zoom, reset",
+				background_fill_color="#F2F2F2")
+	   
+	# add a line renderer
+	p.line(x='x', y='y', source=source, legend_label="Price History", line_color="royalblue", line_width=2)
+	p.circle('x', y='y', source=source, legend_label="Price History",  color="grey", size=2)
+
+	p.add_tools(hover)
+
+	#Formatting
 	p.title.text_font_size			  = '20pt'
 	p.axis.major_label_text_font_size = '15pt'
 	p.axis.axis_label_text_font_style = 'bold italic'
 	p.xaxis.axis_label_text_font_size = "16pt"
 	p.yaxis.axis_label_text_font_size = "16pt"
 	
-	  
 	# show the results
-	show(p)
+	show(p)	
+
 	return
